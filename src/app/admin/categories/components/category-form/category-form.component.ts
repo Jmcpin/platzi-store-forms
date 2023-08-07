c6e31da71bd52f8e61a6f230c61189f89ca564e6
pablo <pablo_pan_da@hotmail.com>
2023-08-07 06:48:51 -0600

    Curso Angular Forms - Creacion y Optimizacion de formularios Web - 21 Proyecto - formulario para crear categorias de productos

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.builderFormm();
+   }
 
   ngOnInit(): void {
+  }
+
+  private builderFormm(){
+    this.formulario = this.formBuilder.group({
+      name: ['',Validators.required],
+      image: ['',Validators.required]
+    });
+  }
+
+  public save(){
+    console.log(this.formulario);
+  }
+
+  get nameField(){
+    return this.formulario.get('name');
+  }
+
+  get imageField(){
+    return this.formulario.get('image');
   }
 
 }
